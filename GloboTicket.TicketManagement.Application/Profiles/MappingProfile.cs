01291265fca0d063a42992f22a6c5212c205249c
Layug Jesriel D <layugjesriel39@gmail.com>
2024-11-11 08:36:49 -0800

    added category feature

--- GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
+++ GloboTicket.TicketManagement.Application/Profiles/MappingProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvents;
 using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventDetail;
 using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsList;
 using GloboTicket.TicketManagement.Domain.Entities;
@@ -17,6 +19,9 @@ namespace GloboTicket.TicketManagement.Application.Profiles
 			CreateMap<Event, EventListVm>().ReverseMap();
 			CreateMap<Event, EventDetailVm>().ReverseMap();
 			CreateMap<Category, CategoryDto>().ReverseMap();
+
+			CreateMap<Category, CategoryListVm>().ReverseMap();
+			CreateMap<Category, CategoryEventListVm>().ReverseMap();
 		}
 	}
 }
