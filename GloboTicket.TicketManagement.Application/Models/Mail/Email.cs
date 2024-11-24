e27f850a1c02ebf5b79ab5a60557ed615b7c5d05
Layug Jesriel D <layugjesriel39@gmail.com>
2024-11-24 09:15:44 -0800

    added email service, email class, and email settings

---
+++ GloboTicket.TicketManagement.Application/Models/Mail/Email.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Models.Mail
+{
+    /*
+     *  Why did we not put this model on the Domain folder?
+     *  
+     *  - This is because Email class has nothing to do with domain that GloboTicket works with.
+     *  - It just a class and a type, and we need to define under application project for the email service to work correctly.
+    */
+
+    public class Email
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
